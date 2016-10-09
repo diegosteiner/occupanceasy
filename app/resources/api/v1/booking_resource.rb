6aeb05823a1974de524de116c845094a79967e94
Diego Steiner <diego.steiner@renuo.ch>
2016-10-10 01:06:52 +0200

    Implemented filters for bookings

--- app/resources/api/v1/booking_resource.rb
+++ app/resources/api/v1/booking_resource.rb
@@ -4,6 +4,29 @@ module Api
     class BookingResource < ApplicationResource
       attributes :begins_at, :ends_at, :blocking
       has_one :occupiable
+
+      filter :begins_after, default: Time.zone.now.beginning_of_month,
+        verify: ->(values, context) { 
+        verify_date_filter(values, context) 
+      },
+      apply: -> (records, value, _options) {
+        records.where(Booking.arel_table[:begins_at].gteq(value.last))
+      }
+
+      filter :ends_before, default: Time.zone.now.beginning_of_month + 1.year,
+
+        verify: ->(values, context) { 
+        verify_date_filter(values, context) 
+      },
+      apply: -> (records, value, _options) {
+        records.where(Booking.arel_table[:ends_at].lteq(value.last))
+      }
+
+      private
+
+      def verify_date_filter(values, context)
+        values.map {|value| Time.iso8601(value) }
+      end
     end
   end
 end
--- spec/api/v1/occupiables_spec.rb
+++ spec/api/v1/occupiables_spec.rb
@@ -8,6 +8,11 @@ def headers
   }
 end
 
+def parsed_json; subject; JSON.parse(response.body); end
+def data; parsed_json['data']; end
+def data_ids; data.map { |item| item['id'] }; end;
+def relationsships; data['relationships']; end
+
 def occupiables_path(id = nil, relationships = nil, params = {})
   path = String.new('/api/v1/occupiables')
   path << '/' << id.to_s if id.present?
@@ -16,38 +21,71 @@ def occupiables_path(id = nil, relationships = nil, params = {})
   path
 end
 
-describe Api::V1::BookingsController, type: :request do
+shared_examples 'valid response' do
+  it  do
+    expect(subject)
+    expect(response).to be_ok
+    expect(response.content_type).to eq('application/vnd.api+json')
+  end
+end
+
+shared_examples 'valid bookings' do
+  it { expect(data_ids).to contain_exactly(*bookings.map(&:id)) }
+end
+
+describe Api::V1::OccupiablesController, type: :request do
   let(:occupiable) { create(:home) }
-  subject(:parsed_json) { JSON.parse(response.body) }
-  let(:data) { parsed_json['data'] }
-  let(:relationsships) { data['relationships'] }
+  let(:params) { {} }
 
   describe '#index' do
-    before { get(occupiables_path, headers: headers) }
+    subject { get(occupiables_path, headers: headers) }
 
-    it do
-      expect(response).to be_ok
-      expect(response.content_type).to eq('application/vnd.api+json')
-    end
+    it_behaves_like 'valid response'
   end
 
   describe '#show' do
-    before { get(occupiables_path(occupiable.to_param), headers: headers) }
+    subject { get(occupiables_path(occupiable.to_param), headers: headers) }
 
-    it do
-      expect(response).to be_ok
-      expect(response.content_type).to eq('application/vnd.api+json')
-    end
+    it_behaves_like 'valid response'
   end
 
   describe '#show/bookings' do
-    let!(:bookings) { create_list(:reservation, 3, occupiable: occupiable) }
-    before { get(occupiables_path(occupiable.to_param, :bookings), headers: headers) }
+    subject { get(occupiables_path(occupiable.to_param, :bookings, params), headers: headers) }
+    let!(:bookings) { create_list(:reservation, 2, occupiable: occupiable, begins_at: 3.weeks.from_now) }
 
-    it do
-      expect(response).to be_ok
-      expect(response.content_type).to eq('application/vnd.api+json')
-      expect(data.count).to be(bookings.count)
+    it_behaves_like 'valid response'
+    it { expect(data.count).to be(bookings.count) }
+
+    describe 'filter' do
+
+      describe 'begins_after' do
+        let!(:bookings_out_of_range) { create_list(:reservation, 2, occupiable: occupiable, begins_at: 3.months.ago) }
+        let(:params) { { 'filter[begins_after]' => Time.zone.now.beginning_of_month.iso8601 } }
+
+        it_behaves_like 'valid response'
+        it_behaves_like 'valid bookings'
+      end
+
+      describe 'ends_before' do
+        let!(:bookings_out_of_range) { create_list(:reservation, 2, occupiable: occupiable, begins_at: 3.months.from_now) }
+        let(:params) { { 'filter[ends_before]' => 1.month.from_now.end_of_month.iso8601 } }
+
+        it_behaves_like 'valid response'
+        it_behaves_like 'valid bookings'
+      end
+
+      describe 'defaults' do
+        let!(:bookings_out_of_range) do 
+          [
+            create(:reservation, occupiable: occupiable, begins_at: 3.months.ago),
+            create(:reservation, occupiable: occupiable, begins_at: 3.years.from_now)
+          ]
+        end
+        let!(:bookings) { create_list(:reservation, 2, occupiable: occupiable, begins_at: 1.weeks.from_now) }
+
+        it_behaves_like 'valid response'
+        it_behaves_like 'valid bookings'
+      end
     end
   end
 end
